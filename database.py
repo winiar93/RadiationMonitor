a00a22370e16ffccead02330dbefaa49c8329e5a
winiar93 <winiar93@gmail.com>
2026-06-11 23:36:10 +0200

    added get_recent_readings

--- database.py
+++ database.py
@@ -22,7 +22,7 @@ def save_reading(reading: GeigerReading):
     with sqlite3.connect(DB_PATH) as conn:
         conn.execute(
             "INSERT INTO readings (cps, timestamp) VALUES (?, ?)",
-            (reading.cps, reading.timestamp.isoformat())
+            (reading.cps, reading.timestamp.isoformat()),
         )
 
 
@@ -32,3 +32,29 @@ def cleanup_old_rows():
             DELETE FROM readings
             WHERE timestamp < datetime('now', '-24 hours')
         """)
+
+
+def get_recent_readings(limit: int | None = None, hours: int | None = None, ma_window: int = 8):
+    with sqlite3.connect(DB_PATH) as conn:
+        inner = f"""
+            SELECT *,
+                   AVG(cps) OVER (
+                       ORDER BY timestamp
+                       ROWS BETWEEN {ma_window - 1} PRECEDING AND CURRENT ROW
+                   ) AS cps_ma
+            FROM readings
+        """
+        if hours:
+            inner += f" WHERE timestamp >= datetime('now', '-{hours} hours')"
+
+        query = f"SELECT * FROM ({inner}) ORDER BY timestamp DESC"
+
+        if limit:
+            query += f" LIMIT {limit}"
+
+        query = f"SELECT * FROM ({query}) ORDER BY timestamp ASC"
+
+        conn.row_factory = sqlite3.Row
+        cursor = conn.execute(query)
+        rows = cursor.fetchall()
+        return [dict(row) for row in rows]
